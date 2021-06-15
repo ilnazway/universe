dde2420205bef521dc53677eff707530e1c213d1
ilnazway <75731531+ilnazway@users.noreply.github.com>
2021-06-15 19:02:03 +0300

    Генерация квадратов в одном месте

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var $start = document.getElementById('start')
 var $game = document.getElementById('game')
 
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
+
+var score = 0
 
 function startGame() {
   // console.log('Zapusk')
@@ -11,6 +14,27 @@ function startGame() {
   renderBox()
 }
 
+function handleBoxClick(event) {
+  if (event.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
+
 function renderBox() {
-  
+  $game.innerHTML = ''
+
+  var box = document.createElement('div')
+
+  box.style.width = box.style.height = '50px'
+  box.style.position = 'absolute'
+  box.style.top = '50px'
+  box.style.left = '60px'
+  box.style.backgroundColor = 'red'
+  box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
+
+  $game.insertAdjacentElement('afterbegin', box)
+
+
 }
