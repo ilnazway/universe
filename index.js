7c322f986d0ad1ce9962b61f28cff07987b0349d
ilnazway <75731531+ilnazway@users.noreply.github.com>
2021-06-16 15:43:15 +0300

    Генерация квадратов в случайных позициях

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ $game.addEventListener('click', handleBoxClick)
 var score = 0
 
 function startGame() {
+  // console.log(getRandom(100, 30))
   // console.log('Zapusk')
   $game.style.background = '#ffffff'
   $start.classList.add('hide')
@@ -22,19 +23,28 @@ function handleBoxClick(event) {
 }
 
 function renderBox() {
-  $game.innerHTML = ''
+  $game.innerHTML = '' // удаляет внутренние теги у родительского
 
-  var box = document.createElement('div')
+  var box = document.createElement('div') // создаем новый тег
+  var boxSize = getRandom(30, 100)
+  var gameSize = $game.getBoundingClientRect() // узнаем размер поля
+  var maxTop = gameSize.height - boxSize
+  var maxLeft = gameSize.width - boxSize
 
-  box.style.width = box.style.height = '50px'
+  box.style.width = box.style.height = boxSize + 'px'
   box.style.position = 'absolute'
-  box.style.top = '50px'
-  box.style.left = '60px'
+  box.style.top = getRandom(0, maxTop) + 'px' // случайное расположение по высоте
+  box.style.left = getRandom(0, maxLeft) + 'px' // случайное расположение по левому краю
   box.style.backgroundColor = 'red'
   box.style.cursor = 'pointer'
   box.setAttribute('data-box', 'true')
 
-  $game.insertAdjacentElement('afterbegin', box)
+  $game.insertAdjacentElement('afterbegin', box) // вставляем созданный тег
 
 
 }
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
