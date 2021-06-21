edd5ecbb051bdff759f48b53267f9f590c256f5c
ilnazway <75731531+ilnazway@users.noreply.github.com>
2021-06-22 00:54:37 +0300

    Изучал принципы создания объектов Класса

--- part6.js
+++ part6.js
@@ -1,12 +1,39 @@
-function Moto(name, year) {
-  this.name = name
+// function Moto(name, year) {
+//   this.name = name
+//   this.year = year
+// }
+
+// var bmw = new Moto('BMW', 2015)
+
+
+var moto = {
+  model: 'Yamaha',
+  year: 2019
+}
+
+moto.hasOwnProperty('model') // определяет, есть метод(ключ) или нет
+// console.log(moto)
+
+
+function Moto(model, year) { // создание класса
+  this.model = model
   this.year = year
 }
 
-var bmw = new Moto('BMW', 2015) 
+Moto.prototype.getAge = function() {
+  return new Date().getFullYear() - this.year
+}
+
+Moto.prototype.color = 'black'
+Moto.prototype.power = 250
 
 
+var geon = new Moto('GEON', 2013)
+var kawasaki = new Moto('Kawasaki', 2015)
 
+geon.power = 248
+kawasaki.color = 'white'
 
-
+console.log(geon)
+console.log(kawasaki)
 
