310f6914897596053fbf0d0377e2e8b2c7b9f5ae
ilnazway <75731531+ilnazway@users.noreply.github.com>
2021-06-24 00:59:54 +0300

    Привязка контекстов. bind, call, apply

---
+++ part6-54.js
@@ -0,0 +1,31 @@
+var printObject = function(objName, additionalName) {
+  console.log('Printing object:', objName)
+  console.log('Additional argument:', additionalName)
+  for (var key in this) {
+    if (this.hasOwnProperty(key)) {
+      console.log('[' + key + ']: ' + this[key])
+    }
+  }
+}
+
+var person = {
+  name: 'Max',
+  age: 23,
+  job: 'frontend',
+  friend: ['Max', 'Zold']
+}
+
+var car = {
+  name: 'Kia',
+  model: 'Sportage',
+  year: 2018
+}
+
+
+var printPerson = printObject.bind(person)
+printPerson('person')
+
+printObject.call(car, 'Mashina-pushka', 'тестовая строка')
+
+printObject.apply(person, ['Персонаж', 'arg2'])
+
